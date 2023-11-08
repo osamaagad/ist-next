d55cb3ea1c7df9aedcf2c65abf206c7b02aa8e6c
mrosamagad <mr.osamagamal@yahoo.com>
2023-11-08 21:42:48 +0200

    new

---
+++ src/app/products/page.js
@@ -0,0 +1,10 @@
+import Navigation from "@/components/nav";
+
+export default function() {
+    return (
+        <div>
+            <Navigation></Navigation>
+            Hello
+        </div>
+    )
+  }
---
+++ src/components/nav.js
@@ -0,0 +1,9 @@
+export default function Navigation() {
+    return (
+        <div>
+            nav
+
+            <a href="/products" className="">Products</a>
+        </div>
+    )
+  }

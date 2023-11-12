bc73c5493719ba6ad48f0e274c04c9bbcc326ce2
kareem kamal <kareemkamal91@gmail.com>
2023-11-12 22:59:07 +0100

    test

---
+++ src/app/ar/products/solar/page.js
@@ -0,0 +1,128 @@
+import ProductList from "@/components/ProductList";
+
+export default function ProductListPage() {
+  return (
+    <div className="productListPage">
+      <h2 className="productPageTitle">Solar</h2>
+
+      <ProductList
+        products={[
+          {
+            name: 'Product 1',
+            href: '/ar/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'gel battery']
+          },
+          {
+            name: 'Product 2',
+            href: '/ar/products/solar/product-2',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand Y', 'gel battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/ar/products/solar/product-3',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/ar/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/ar/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/ar/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/ar/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/ar/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/ar/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/ar/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/ar/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/ar/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/ar/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/ar/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/ar/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Pro125312duct 3',
+            href: '/ar/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3adfasdf',
+            href: '/ar/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Prasfdadsoduct 3',
+            href: '/ar/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3adsfasdf',
+            href: '/ar/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+        ]}
+      />
+    </div>
+  );
+}

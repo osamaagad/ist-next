a2ba3af3cfc14a2be8d2f7842f4303c1ce2f7124
mrosamagad <97454038+mrosamagad@users.noreply.github.com>
2023-12-06 17:29:58 +0200

    ss

--- src/app/ar/products/education/page.js
+++ src/app/ar/products/education/page.js
@@ -3,22 +3,177 @@ import ProductList from "@/components/ProductList";
 export default function ProductListPage() {
   return (
     <div className="productListPage">
-      <h2>Solar</h2>
+      <h2 className="productPageTitle">Solar</h2>
 
       <ProductList
+        tagItems={{
+          'Battery': {
+            'Gel': {},
+            'Lithium': {}
+          },
+          'Inverter': {
+            'Off Grid': {},
+            'On Grid': {},
+            'Solar Pump':{},
+            'Industerial':{}
+          },
+          'Solar Panels':{},
+          'Solar Lighting':{
+            'T T':{},
+            'Ceilling':{},
+            'Crossbow 4':{},
+            'Crossbow 5':{},
+            'Crossbow 6':{},
+            'Wawa':{},
+            'King light':{},
+
+          },
+          'Solar UPS':{
+            'Power Can':{},
+            'Smart Power':{},
+            'Smart Power':{},
+            'Made In Eygpt':{}
+          },
+          'Mounting System':{},
+          'Cables':{
+            'DC':{},
+            'AC':{}
+          },
+          'Solar Heatting':{
+            'flat plate':{},
+            'evacuated tube':{}
+          },
+          'Tracking System':{
+            'Single Axis':{},
+            'Dual Axis':{}
+          },
+          'Pump':{
+            'Submerged':{},
+            'Superficial':{}
+          },
+          'Protection':{
+            'MCB':{},
+            'MCCB':{},
+            'MC4':{},
+            'Fuse':{},
+            'SPD':{},
+            'Over/Under Voltage':{},
+            'Relay':{},
+            'Contactors':{}
+          }
+        }}
         products={[
           {
             name: 'Product 1',
+            href: '/products/solar/product-1',
             imgUrl: '/prot1.png',
-            tags: ['battery', 'brand X', 'gel battery']
+            tags: ['Battery', 'brand X', 'gel battery']
           },
           {
             name: 'Product 2',
+            href: '/products/solar/product-2',
             imgUrl: '/prot1.png',
             tags: ['battery', 'brand Y', 'gel battery']
           },
           {
             name: 'Product 3',
+            href: '/products/solar/product-3',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['Battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'Gel', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3',
+            href: '/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Pro125312duct 3',
+            href: '/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3adfasdf',
+            href: '/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Prasfdadsoduct 3',
+            href: '/products/solar/product-1',
+            imgUrl: '/prot1.png',
+            tags: ['battery', 'brand X', 'lithium battery']
+          },
+          {
+            name: 'Product 3adsfasdf',
+            href: '/products/solar/product-1',
             imgUrl: '/prot1.png',
             tags: ['battery', 'brand X', 'lithium battery']
           },
--- src/app/ar/products/page.js
+++ src/app/ar/products/page.js
@@ -1,21 +1,21 @@
 export default function ProductsPage() {
     return <div className="productsPage">
-        <a className="productSection" href="/ar/products/solar">
+        <a className="productSection" href="/products/solar">
             <img src="/prot1.png" width="300" height="300" />
             <h2>Solar</h2>
         </a>
         
-        <a className="productSection" href="/ar/products/water">
+        <a className="productSection" href="/products/water">
             <img src="/prot1.png" width="300" height="300" />
             <h2>Water</h2>
         </a>
         
-        <a className="productSection" href="/ar/products/education">
+        <a className="productSection" href="/products/education">
             <img src="/prot1.png" width="300" height="300" />
             <h2>Education</h2>
         </a>
         
-        <a className="productSection" href="/ar/products/firefighting">
+        <a className="productSection" href="/products/firefighting">
             <img src="/prot1.png" width="300" height="300" />
             <h2>Firefighting</h2>
         </a>

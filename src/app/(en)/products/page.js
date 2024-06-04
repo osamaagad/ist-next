7016aade473017a47a793c464d13083e66f035b5
mrosamagad <97454038+mrosamagad@users.noreply.github.com>
2024-06-04 13:35:31 +0300

    kl

--- src/app/(en)/products/page.js
+++ src/app/(en)/products/page.js
@@ -1,22 +1,22 @@
 export default function ProductsPage() {
     return <div className="productsPage">
         <a className="productSection" href="/products/solar">
-            <img src="/prot1.png" width="300" height="300" />
+            <img src="/front-face-of-product/f-f-solar.jpg" width="300" height="300" />
             <h2>Solar</h2>
         </a>
         
         <a className="productSection" href="/products/water">
-            <img src="/front-face-of-product/f-f-solar.jpg" width="300" height="300" />
+            <img src="/front-face-of-product/f-f-water.jpg" width="300" height="300" />
             <h2>Water</h2>
         </a>
         
         <a className="productSection" href="/products/education">
-            <img src="/prot1.png" width="300" height="300" />
+            <img src="/front-face-of-product/f-f-Education.jpg" width="300" height="300" />
             <h2>Education</h2>
         </a>
         
         <a className="productSection" href="/products/Fire">
-            <img src="/prot1.png" width="300" height="300" />
+            <img src="/front-face-of-product/f-f-fire.jpg" width="300" height="300" />
             <h2>Fire Systems</h2>
         </a>
     </div>

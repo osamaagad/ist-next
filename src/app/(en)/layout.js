6bbffed85e2f3856687d184f7eab27e6df95e9ae
Kareem <kareemkamal91@gmail.com>
2024-12-28 15:39:43 +0100

    analytics

--- src/app/(en)/layout.js
+++ src/app/(en)/layout.js
@@ -16,6 +16,14 @@ export default function Layout({ children }) {
         <script src="/js/bootstrap.bundle.min.js"></script>
         <script src="/js/jquery-3.0.0.min.js"></script>
         <script src="/js/custom.js"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-TC39K8PXC5"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'G-TC39K8PXC5');
+        </script>
       </head>
       <body className={`${inter.className} main-layout`}>
         <Navigation />

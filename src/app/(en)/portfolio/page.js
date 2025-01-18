4beadf9f47a57b9423201c8b03a645699b5b06dc
mrosamagad <mr.osamagamal@yahoo.com>
2025-01-18 20:39:30 +0200

    kjhu

--- src/app/(en)/portfolio/page.js
+++ src/app/(en)/portfolio/page.js
@@ -1,22 +1,22 @@
 export default function ProductsPage() {
     return <div className="productsPage">
         <a className="productSection" href="/portfolio/solar">
-            <img src="/prot1.png" width="300" height="300" />
+            <img src="/solarenergy-projact.jpg" width="300" height="300" />
             <h2>Solar Energy Projects</h2>
         </a>
         
         <a className="productSection" href="/portfolio/water">
-            <img src="/prot1.png" width="300" height="300" />
+            <img src="/Water-Treatment-Projects.jpg" width="300" height="300" />
             <h2>Water Treatment Projects</h2>
         </a>
         
         <a className="productSection" href="/portfolio/Educational">
-            <img src="/prot1.png" width="300" height="300" />
+            <img src="/Education-Technology-Projects.png" width="300" height="300" />
             <h2>Education Technology Projects</h2>
         </a>
         
         <a className="productSection" href="/portfolio/fire">
-            <img src="/prot1.png" width="300" height="300" />
+            <img src="/Fire-Project.jpg" width="300" height="300" />
             <h2>Fire Projects</h2>
         </a>
     </div>
